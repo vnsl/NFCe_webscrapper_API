e4b47c9734b5896c65894a957f8e5bd7622bcc58
vnsl <viniciusnattan14@gmail.com>
2022-11-30 15:26:18 -0300

    Heroku test3

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,11 @@ import express from 'express';
 import routes from './routes';
 
 const app = express();
+const cors = require('cors');
 
 app.use(express.json());
+app.use(cors());
 
 app.use(routes);
 
-app.listen(3000)
-
-module.exports = app;
+app.listen(process.env.PORT || 3000)
