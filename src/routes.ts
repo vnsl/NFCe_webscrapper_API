6b04dc31b60ed69ab49a365a49f9c4089f10a8fd
vnsl <viniciusnattan14@gmail.com>
2022-11-30 14:58:38 -0300

    Heroku test

--- src/routes.ts
+++ src/routes.ts
@@ -4,5 +4,6 @@ import { NFCeController } from "./controllers/NFCeController";
 const routes = Router();
 
 routes.get('/', new NFCeController().getInfo)
+routes.post('/teste', new NFCeController().getInfo)
 
 export default routes;
