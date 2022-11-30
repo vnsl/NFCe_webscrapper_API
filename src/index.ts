bda0e30b6d4a9b839d53d0db2f0d24a5907fe947
vnsl <viniciusnattan14@gmail.com>
2022-11-30 16:01:51 -0300

    Update on cors policy 3

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,12 @@
 import express from 'express';
 import routes from './routes';
-const cors = require('cors');
 
+const cors = require('cors');
 const app = express();
 
 app.use(express.json());
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
-    app.use(cors());
-    next();
-});
+app.use(cors());
 
 app.use(routes);
 
-app.listen(process.env.PORT || 3000)
+app.listen(process.env.PORT || 8000)
